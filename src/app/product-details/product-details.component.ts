b132101727e0d873f106a2d09194bbcff40a6659
StefanyEdu <ostefany20@gmail.com>
2021-10-27 19:58:24 -0300

    em andamento

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -3,17 +3,24 @@ import { ActivatedRoute } from '@angular/router';
 
 import { Product, products } from '../products';
 
+@Component({
+  selector: 'app-details',
+  templateUrl: 'details.component.html',
+  styleUrls: ['details.component.css'],
+})
 export class ProductDetailsComponent implements OnInit {
   product: Product | undefined;
 
   constructor(private route: ActivatedRoute) {}
-}
 
-ngOnInit() {
-  // First get the product id from the current route.
-  const routeParams = this.route.snapshot.paramMap;
-  const productIdFromRoute = Number(routeParams.get('productId'));
+  ngOnInit() {
+    // First get the product id from the current route.
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
 
-  // Find the product that correspond with the id provided in route.
-  this.product = products.find(product => product.id === productIdFromRoute);
+    // Find the product that correspond with the id provided in route.
+    this.product = products.find(
+      (product) => product.id === productIdFromRoute
+    );
+  }
 }
