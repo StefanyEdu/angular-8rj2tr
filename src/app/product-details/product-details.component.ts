1270f2f76efd36a01be7a23207fbbb3f516a584e
StefanyEdu <ostefany20@gmail.com>
2021-10-28 13:54:56 -0300

    Inicio do projeto

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -5,8 +5,8 @@ import { Product, products } from '../products';
 
 @Component({
   selector: 'app-details',
-  templateUrl: 'details.component.html',
-  styleUrls: ['details.component.css'],
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.css'],
 })
 export class ProductDetailsComponent implements OnInit {
   product: Product | undefined;
